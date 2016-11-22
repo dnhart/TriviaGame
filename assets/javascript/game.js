9a80b601a667e771693fdf0b995e8624c92892b6
FBIMOBILEUNIT\D'Ann <me@dnhart.com>
2016-11-22 17:00:56 -0600

    starting code. works, but not all functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,157 @@
+//starter variables
+var correct = 0;
+var incorrect = 0;
+
+
+
+//questions, links, and answers.
+
+var questions = {
+	painting1: {
+		title: "Dogs Playing Poker",
+		painting: "src='assets/images/dogs.jpg'",
+		choices: ["Wassily Kandinsky", "C.M. Coolidge", "Peter Paul Rubens", "Sandro Botticelli"],
+		artist: "C.M. Coolidge"
+	},
+	painting2: {
+		title: "No. 5, 1948",
+		painting: "src='assets/images/number-5.jpg'",
+		choices: ["Jackson Polluck","Pablo Picasso","Peter Paul Rubens","Grant Wood"],
+		artist: "Jackson Polluck"
+	},
+	painting3:{
+		title: "The Son of Man",
+		painting: "src='assets/images/sonofman.jpg'",
+		choices: ["Francisco Goya", "Diego Velazquez", "Rene Magrittees","Jan van Eyck"],
+		artist: "Rene Magrittees"
+	},
+	// painting4: {
+	// 	title: "A Sunday Afternoon on the Island of La Grande Jatte",
+	// 	painting: "src='assets/images/lagrande.jpg'",
+	// 	choices: ["Edouard Manet","Georges Suerat", "Edgar Degas", "Claude Monet"],
+	// 	artist: "Georges Suerat"
+	// },
+	// painting5: {
+	// 	title: "American Gothic",
+	// 	painting: "src='assets/images/gothic.jpg'",
+	// 	choices: ["Grant Wood",  "Henry Matisse", "Diego Rivera", "Diego Velazquez"],
+	// 	artist: "Grant Wood"
+	// },
+	// painting6: {
+	// 	title: "The Persistence of Memory",
+	// 	painting: "src='assets/images/memory.jpg'",
+	// 	choices: ["Salvador Dali", "Vincent Van Gogh", "Pablo Picasso", "Claude Monet"], 
+	// 	artist: "Salvador Dali"
+	// },
+	// painting7: {
+	// 	title: "Starry Night",
+	// 	painting: "src='assets/images/starrynight.jpg'",
+	// 	choices: ["Mark Rothko", "Raphael","Claude Monet", "Vincent Van Gogh"],
+	// 	artist: "Vincent Van Gogh"
+	// },
+	// painting8: {
+	// 	title: "Girl with a Pearl Earring",
+	// 	painting: "src='assets/images/pearl.jpg'",
+	// 	choices: ["Gustav Klimt", "Renoir","Johannes Vermeer", "Francisco Goya"],
+	// 	artist: "Johannes Vermeer"
+	// },
+	// painting9: {
+	// 	title: "Night Watch",
+	// 	painting:"src='assets/images/nightwatch.jpg'",
+	// 	choices: ["Renoir", "Rembrandt", "Caravaggio", "Michelangelo"],
+	// 	artist: "Rembrandt"
+	// },
+	// painting10: {
+	// 	title:"School of Athens",
+	// 	painting: "src='assets/images/athens.jpg'",
+	// 	choices: ["Raphael", "Michelangelo","Leonardo da Vinci", "Rembrandt"],
+	// 	artist: "Raphael",
+	// }
+};
+//var artists is for if I wanted to dynamically created the choices.
+//var artists ={"C.M. Coolidge", "Jackson Polluck", "Rene Magrittees", "Georges Suerat", "Grant Wood", "Salvador Dali", "Vincent Van Gogh", "Johannes Vermeer", "Rembrandt", "Raphael", "Sandro Botticelli", "Mark Rothko", "Peter Paul Rubens", "Henry Matisse", "Diego Rivera", "James McNeill Whistler", "Pablo Picasso", "Wassily Kandinsky", "Gustav Klimt", "Renoir", "Edouard Manet", "Francisco Goya", "Diego Velazquez", "Jan van Eyck", "Edvard Munch", "Claude Monet", "Michelangelo", "Caravaggio", "Leonardo da Vinci", "Edgar Degas"};
+
+var showQuestion;
+var indexCount = 0;
+var arrayLength = Object.keys(questions).length;
+//start quiz from start button
+$("#start").on("click", startQuiz);
+
+function startQuiz(){
+	$("#timerBox").html("<div class='row'><div class='col-xs-12' id='display'>10</div></div>");
+	$("#quizActive").toggle();
+	$("#scoreboard").toggle();
+	displayQuestion();
+	
+};
+
+function displayQuestion(){
+
+		var paintingContainer=$("#paintingContainer");
+//set specific variables
+		var paintingNumber = Object.keys(questions)[indexCount];
+		var paintingTitle = questions[paintingNumber]["title"];
+		var paintingLocation = "<img class='img-responsive' alt='"+paintingTitle+"'" +questions[paintingNumber]["painting"]+" />";
+		var paintingChoices = questions[paintingNumber]["choices"];
+		var paintingArtist = questions[paintingNumber]["artist"];
+		console.log(paintingNumber, paintingTitle, paintingLocation, paintingChoices, paintingArtist);
+
+	
+//update html - painting, choices, add/change value to choices
+		$("#paintingTitle").html(paintingTitle);
+		$("#paintingContainer").html(paintingLocation);
+			for(var i=0; i<paintingChoices.length; i++){
+				$("#option"+i).html(paintingChoices[i]);
+				$("#option"+i).attr("value", paintingChoices[i]);
+			};
+
+		timer();
+
+}; //end displayQuestion loop
+
+
+
+function nextQuestion(){
+	indexCount++;
+	
+	if (indexCount >= arrayLength) {
+  		console.log("quiz done");
+  	}else{
+  		displayQuestion();
+  	};
+};
+
+
+function stopTime() {
+
+    	time=11;
+    	clearInterval(counter);
+    	//setTimeout(nextQuestion, 3000);
+
+};
+
+function checkAnswer(){
+	var guess = this.attributes[2].value;
+	console.log(guess);
+	stopTime();
+}
+
+
+function timer(){
+	var time= 10;
+	counter = setInterval(countdown, 1000);
+	$(".choices").on("click", checkAnswer);
+	function countdown() {
+		if (time>11){
+			time--;
+		} else if(time>0 && time<=11){
+    	time--;
+    	$("#display").html(time);
+    	} else {
+			$("#paintingContainer").html("<img class='img-responsive' alt='Time is up' src='assets/images/timesup.jpg' />");
+    		$("#display").html("");
+    		stopTime();
+    	};   
+	}; //end countdown
+
+};//end timer
