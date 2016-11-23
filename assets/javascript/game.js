e288ff98596d729f1b15e789299f7065457b6141
FBIMOBILEUNIT\D'Ann <me@dnhart.com>
2016-11-22 21:25:51 -0600

    working quiz. restart works. basic error precautions. css needs to be done. functional but not pretty

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,3 @@
-//starter variables
-var correct = 0;
-var incorrect = 0;
-
 
 
 //questions, links, and answers.
@@ -25,52 +21,56 @@ var questions = {
 		choices: ["Francisco Goya", "Diego Velazquez", "Rene Magrittees","Jan van Eyck"],
 		artist: "Rene Magrittees"
 	},
-	// painting4: {
-	// 	title: "A Sunday Afternoon on the Island of La Grande Jatte",
-	// 	painting: "src='assets/images/lagrande.jpg'",
-	// 	choices: ["Edouard Manet","Georges Suerat", "Edgar Degas", "Claude Monet"],
-	// 	artist: "Georges Suerat"
-	// },
-	// painting5: {
-	// 	title: "American Gothic",
-	// 	painting: "src='assets/images/gothic.jpg'",
-	// 	choices: ["Grant Wood",  "Henry Matisse", "Diego Rivera", "Diego Velazquez"],
-	// 	artist: "Grant Wood"
-	// },
-	// painting6: {
-	// 	title: "The Persistence of Memory",
-	// 	painting: "src='assets/images/memory.jpg'",
-	// 	choices: ["Salvador Dali", "Vincent Van Gogh", "Pablo Picasso", "Claude Monet"], 
-	// 	artist: "Salvador Dali"
-	// },
-	// painting7: {
-	// 	title: "Starry Night",
-	// 	painting: "src='assets/images/starrynight.jpg'",
-	// 	choices: ["Mark Rothko", "Raphael","Claude Monet", "Vincent Van Gogh"],
-	// 	artist: "Vincent Van Gogh"
-	// },
-	// painting8: {
-	// 	title: "Girl with a Pearl Earring",
-	// 	painting: "src='assets/images/pearl.jpg'",
-	// 	choices: ["Gustav Klimt", "Renoir","Johannes Vermeer", "Francisco Goya"],
-	// 	artist: "Johannes Vermeer"
-	// },
-	// painting9: {
-	// 	title: "Night Watch",
-	// 	painting:"src='assets/images/nightwatch.jpg'",
-	// 	choices: ["Renoir", "Rembrandt", "Caravaggio", "Michelangelo"],
-	// 	artist: "Rembrandt"
-	// },
-	// painting10: {
-	// 	title:"School of Athens",
-	// 	painting: "src='assets/images/athens.jpg'",
-	// 	choices: ["Raphael", "Michelangelo","Leonardo da Vinci", "Rembrandt"],
-	// 	artist: "Raphael",
-	// }
+	painting4: {
+		title: "A Sunday Afternoon on the Island of La Grande Jatte",
+		painting: "src='assets/images/lagrande.jpg'",
+		choices: ["Edouard Manet","Georges Suerat", "Edgar Degas", "Claude Monet"],
+		artist: "Georges Suerat"
+	},
+	painting5: {
+		title: "American Gothic",
+		painting: "src='assets/images/gothic.jpg'",
+		choices: ["Grant Wood",  "Henry Matisse", "Diego Rivera", "Diego Velazquez"],
+		artist: "Grant Wood"
+	},
+	painting6: {
+		title: "The Persistence of Memory",
+		painting: "src='assets/images/memory.jpg'",
+		choices: ["Salvador Dali", "Vincent Van Gogh", "Pablo Picasso", "Claude Monet"], 
+		artist: "Salvador Dali"
+	},
+	painting7: {
+		title: "Starry Night",
+		painting: "src='assets/images/starrynight.jpg'",
+		choices: ["Mark Rothko", "Raphael","Claude Monet", "Vincent Van Gogh"],
+		artist: "Vincent Van Gogh"
+	},
+	painting8: {
+		title: "Girl with a Pearl Earring",
+		painting: "src='assets/images/pearl.jpg'",
+		choices: ["Gustav Klimt", "Renoir","Johannes Vermeer", "Francisco Goya"],
+		artist: "Johannes Vermeer"
+	},
+	painting9: {
+		title: "Night Watch",
+		painting:"src='assets/images/nightwatch.jpg'",
+		choices: ["Renoir", "Rembrandt", "Caravaggio", "Michelangelo"],
+		artist: "Rembrandt"
+	},
+	painting10: {
+		title:"School of Athens",
+		painting: "src='assets/images/athens.jpg'",
+		choices: ["Raphael", "Michelangelo","Leonardo da Vinci", "Rembrandt"],
+		artist: "Raphael",
+	}
 };
 //var artists is for if I wanted to dynamically created the choices.
 //var artists ={"C.M. Coolidge", "Jackson Polluck", "Rene Magrittees", "Georges Suerat", "Grant Wood", "Salvador Dali", "Vincent Van Gogh", "Johannes Vermeer", "Rembrandt", "Raphael", "Sandro Botticelli", "Mark Rothko", "Peter Paul Rubens", "Henry Matisse", "Diego Rivera", "James McNeill Whistler", "Pablo Picasso", "Wassily Kandinsky", "Gustav Klimt", "Renoir", "Edouard Manet", "Francisco Goya", "Diego Velazquez", "Jan van Eyck", "Edvard Munch", "Claude Monet", "Michelangelo", "Caravaggio", "Leonardo da Vinci", "Edgar Degas"};
 
+
+//starter variables
+var correct = 0;
+var incorrect = 0;
 var showQuestion;
 var indexCount = 0;
 var arrayLength = Object.keys(questions).length;
@@ -117,7 +117,7 @@ function nextQuestion(){
 	indexCount++;
 	
 	if (indexCount >= arrayLength) {
-  		console.log("quiz done");
+  		summaryPage();
   	}else{
   		displayQuestion();
   	};
@@ -176,3 +176,50 @@ function timer(){
 	}; //end countdown
 
 };//end timer
+
+
+function summaryPage(){
+	$("#timerBox").html("<button id='start'>Play Again!</button>");
+	$("#start").on("click", restartQuiz);
+	$("#quizActive").toggle();
+	$("#scoreboard").toggle();
+	$("#summaryActive").toggle();
+
+	var percentRight = Math.round((correct/(correct+incorrect))*100);
+
+	if(percentRight=== 100){
+		$("#summaryTitle").html("You are awesome!");
+	} else if(percentRight >=90 && percentRight < 100){
+		$("#summaryTitle").html("Excellent Job!");
+	} else if(percentRight >=70 && percentRight < 90){
+		$("#summaryTitle").html("Good Job!");
+	} else if(percentRight >=40 && percentRight < 70){
+		$("#summaryTitle").html("You might want to review a bit...");
+	} else {
+		$("#summaryTitle").html("Don't quit your day job...");
+	}; 
+
+	$("#summaryContainer").html("<div class='fullwidth'>You got a....</div><div class='finalpercent'>"+percentRight+"%</div><div class='fullwidth'><p>Correct answers: "+correct+"</p><p>Incorrect answers: "+incorrect+"</p></div>");
+
+
+};
+
+function restartQuiz(){
+//reset starter variables
+correct = 0;
+incorrect = 0;
+showQuestion;
+indexCount = 0;
+
+//show timerbox, toggle quizActive, toggle scoreboard, toggle summaryActive
+	$("#timerBox").html("<div class='row'><div class='col-xs-12' id='display'>10</div></div>");
+	$("#quizActive").toggle();
+	$("#scoreboard").toggle();
+	$("#summaryActive").toggle();
+	$("#incorrectTotal").html(incorrect);
+	$("#correctTotal").html(correct);
+
+//start displayQuestion
+	displayQuestion();
+	
+};
